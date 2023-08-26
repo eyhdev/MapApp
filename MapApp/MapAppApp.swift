f09ba4925282934c948011d976f4eb956d84ecf3
eyhdev <alaqsa335@gmail.com>
2023-08-26 13:27:35 +0300

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by eyh.mac on 26.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
