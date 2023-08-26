9015874fbf5b888c4b8ccd8d820c875b8ae6c52f
eyhdev <alaqsa335@gmail.com>
2023-08-26 13:49:18 +0300

    Map App SwiftUI

--- MapApp/ContentView.swift
+++ MapApp/ContentView.swift
@@ -6,16 +6,77 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    
+    @State private var location: [MKPointAnnotation] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            MapView(centerCoordinate: $centerCoordinate, annotations: location)
+                .edgesIgnoringSafeArea(.all)
+            
+            Circle()
+                .background(.ultraThinMaterial)
+                .clipShape(Circle())
+                .frame(width: 32, height: 32)
+            
+            VStack{
+                Spacer()
+                Button(action: {
+                    
+                    let newLocation = MKPointAnnotation()
+                    newLocation.coordinate = self.centerCoordinate
+                    self.location.append(newLocation)
+                    
+                }) {
+                    Image(systemName: "plus")
+                        .padding()
+                        .background(.ultraThinMaterial)
+                        .foregroundColor(.black)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
+                }
+            }
         }
-        .padding()
+    }
+    
+    struct MapView: UIViewRepresentable {
+        @Binding var centerCoordinate: CLLocationCoordinate2D
+        var annotations: [MKPointAnnotation]
+        
+        func makeUIView(context: Context) -> MKMapView {
+            let mapView = MKMapView()
+            mapView.delegate = context.coordinator
+            return mapView
+        }
+        
+        func updateUIView(_ uiView: MKMapView, context: Context) {
+            if annotations.count != uiView.annotations.count {
+                uiView.removeAnnotations(uiView.annotations)
+                uiView.addAnnotations(annotations)
+            }
+        }
+        
+        func makeCoordinator() -> Coordinator {
+            Coordinator(self)
+        }
+        
+        class Coordinator: NSObject, MKMapViewDelegate {
+            var parent: MapView
+            
+            init(_ parent: MapView) {
+                self.parent = parent
+            }
+            
+            func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+                parent.centerCoordinate = mapView.centerCoordinate
+            }
+        }
     }
 }
 
